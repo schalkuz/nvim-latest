f53cccd8eef65a60e56ce1e04a07c9ebc789534a
Schalk Conradie <schalkuz.sc@gmail.com>
2025-02-28 14:41:54 +0200

    feat: initial commit
    Set up a few basics and C# configs

---
+++ lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+}
